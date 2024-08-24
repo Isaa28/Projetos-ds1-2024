ad983a2a567a8392d0ae1a603e4712ecd2ded67a
Isaa28 <isadora.silva112@aluno.ce.gov.br>
2024-08-24 16:35:00 -0300

    Atualizando as questões.

--- Log-prog/ativ-19-08/2q.py
+++ Log-prog/ativ-19-08/2q.py
@@ -1,4 +1,4 @@
-for n in range (1,100):
+for n in range (1,101):
     if n % 2 == 1:
         continue
     print(f"{n}")
---
+++ Log-prog/ativ-23-08/1q.py
@@ -0,0 +1,44 @@
+op = " "
+while op == " " :
+    print(""" 
+    -----------Calc.py----------
+    Escolha uma operação:
+    1-Soma
+    2-Subtração
+    3-Multiplicação
+    4-Divisão
+    5-Elevado ao quadrado
+    0-sair
+    """)
+    op = input("Digite sua operação: ")
+    if op not in ["1" , "2" , "3" , "4" , "5" , "0"]:
+        print("Operação inválida, tente novamente.")
+        op = " "
+        continue
+    else:
+        if op == "1":
+            print("sua operação é soma.")
+            n1 = int(input("Digite seu primeiro número: "))
+            n2 = int(input("Digite outro número: "))
+            print(f"O resultado da sua operação é {n1 + n2}.")
+        if op == "2":
+            print("sua operação é subtração.")
+            n1 = int(input("Digite seu primeiro número: "))
+            n2 = int(input("Digite outro número: "))
+            print(f"O resultado da sua operação é {n1 - n2}.")
+        if op == "3":
+            print("sua operação é multiplicação.")
+            n1 = int(input("Digite seu primeiro número: "))
+            n2 = int(input("Digite outro número: "))
+            print(f"O resultado da sua operação é {n1 * n2}.")
+        if op == "4":
+            print("sua operação é divisão.")
+            n1 = int(input("Digite seu primeiro número: "))
+            n2 = int(input("Digite outro número: "))
+            print(f"O resultado da sua operação é {n1 / n2}.")
+        if op == "5":
+            print("sua operação é elevar um número ao quadrado.")
+            n1 = int(input("Digite seu número: "))
+            print(f"O resultado da sua operação é {n1 * n1}.")
+        if op == "0":
+            break
