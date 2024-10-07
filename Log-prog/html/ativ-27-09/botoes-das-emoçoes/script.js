3c4d2b2e9b525c2bc7f7c1a7d64a8d868b320c9f
Isaa28 <isadora.silva112@aluno.ce.gov.br>
2024-10-07 14:50:10 -0300

    Atividade do Hugo JS.

---
+++ Log-prog/html/ativ-27-09/botoes-das-emoçoes/script.js
@@ -0,0 +1,9 @@
+function feliz(){
+    document.getElementById('emocao').innerHTML="😁"
+}
+function sla(){
+    document.getElementById('emocao').innerHTML="😐"
+}
+function triste(){
+    document.getElementById('emocao').innerHTML="😭"
+}
---
+++ Log-prog/html/ativ-27-09/oque-voce-digitou/script.js
@@ -0,0 +1,4 @@
+function exibir(){
+    const informacao = document.getElementById("entrada").value;
+    document.getElementById("Texto").innerText = informacao;
+}
